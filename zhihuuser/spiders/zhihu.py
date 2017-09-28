7d05655355c8ec80995b904d16dfcf14cab93583
cwx2810 <cwx2810@163.com>
2017-09-28 08:20:32 -0700

    拼接url获取用户信息和其关注用户信息

--- zhihuuser/spiders/zhihu.py
+++ zhihuuser/spiders/zhihu.py
@@ -7,10 +7,23 @@ class ZhihuSpider(Spider):
     allowed_domains = ['www.zhihu.com']
     start_urls = ['http://www.zhihu.com/']
 
-    def start_requests(self):
-        # url = 'https://www.zhihu.com/api/v4/members/l-wt?include=allow_message%2Cis_followed%2Cis_following%2Cis_org%2Cis_blocking%2Cemployments%2Canswer_count%2Cfollower_count%2Carticles_count%2Cgender%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics'
-        url = 'https://www.zhihu.com/api/v4/members/excited-vczh/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=40&limit=20'
-        yield Request(url, callback=self.parse)
+    start_user = 'excited-vczh'
 
-    def parse(self, response):
+    # 拼接用户信息接口url
+    user_url = 'https://www.zhihu.com/api/v4/members/{user}?include={include}'
+    user_query = 'allow_message,is_followed,is_following,is_org,is_blocking,employments,answer_count,follower_count,articles_count,gender,badge[?(type=best_answerer)].topics'
+
+    # 拼接关注接口url
+    follows_url = 'https://www.zhihu.com/api/v4/members/{user}/followees?include={include}&offset={offset}&limit={limit}'
+    follows_query = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
+
+    def start_requests(self):
+        # 实现构造url
+        yield Request(self.user_url.format(user=self.start_user, include=self.user_query), self.parse_user)
+        yield Request(self.follows_url.format(user=self.start_user, include=self.follows_query, offset=0, limit=20), callback=self.parse_follows)
+
+    def parse_user(self, response):
+        print(response.text)
+
+    def parse_follows(self, response):
         print(response.text)
