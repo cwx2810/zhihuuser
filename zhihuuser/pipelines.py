2bd67505182659f1fbb36a13363f11b71c1a51bb
cwx2810 <cwx2810@163.com>
2017-10-01 08:20:02 -0700

    把数据保存到mongodb

--- zhihuuser/pipelines.py
+++ zhihuuser/pipelines.py
@@ -4,8 +4,29 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
 
+class MongoPipeline(object):
 
-class ZhihuuserPipeline(object):
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        # 去重，如果查找到就更新，查不到就插入
+        self.db['user'].update({'url_token': item['url_token']},{'$set': item}, True)
         return item
--- zhihuuser/spiders/zhihu.py
+++ zhihuuser/spiders/zhihu.py
@@ -23,7 +23,9 @@ class ZhihuSpider(Spider):
 
     def start_requests(self):
         # 实现构造用户自己和其关注的人的json的url
+        # 请求轮子自己本身的json列表
         yield Request(self.user_url.format(user=self.start_user, include=self.user_query), self.parse_user)
+        # 请求轮子关注的人的json列表
         yield Request(self.follows_url.format(user=self.start_user, include=self.follows_query, offset=0, limit=20), callback=self.parse_follows)
 
     def parse_user(self, response):
@@ -41,11 +43,11 @@ class ZhihuSpider(Spider):
     def parse_follows(self, response):
         # 将返回的轮子哥关注的人的json输出
         results = json.loads(response.text)
-
+        # 先解析
         if 'data' in results.keys():
             for result in results.get('data'):
                 yield Request(self.user_url.format(user=result.get('url_token'), include=self.user_query), self.parse_user)
-
+        # 再分页
         if 'paging' in results.keys() and results.get('paging').get('is_end') == False:
             next_page = results.get('paging').get('next')
             yield Request(next_page, self.parse_follows)
